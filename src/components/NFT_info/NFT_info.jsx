b539cad9e139eb58c0168005700ff06fb7f7f802
hoapham <20520995@gm.uit.edu.vn>
2022-12-07 12:13:13 +0700

    hoaUI fix hover transittion

--- src/components/NFT_info/NFT_info.jsx
+++ src/components/NFT_info/NFT_info.jsx
@@ -17,100 +17,91 @@ function toBase64(arr) {
   return btoa(arr.reduce((data, byte) => data + String.fromCharCode(byte), ""));
 }
 
-const NFTItemStyled = styled(Card)`               
-width: 300px;
+const NFTItemStyled = styled(Card)`
+  width: 300px;
 
-.wrapper{
-
-  width: 100%;
-  height: 100%;
-  
-
-  &:hover{
-    .cardButton{
-      top: 0;
-        }
-
-  }
-}
-
-
-
-.cardContainer {
-  display: flex;
-  flex-direction: column;
- 
-  .cardImg {
+  .wrapper {
     width: 100%;
-    height: 300px;
-    overflow: hidden;
+    height: 100%;
 
-    .img {
-      width: 100%;
-      height: 100%;
-      object-fit: cover;
-
-      &:hover {
-        transform: scale(1.1);
-        transition: all 0.5s ease;
+    &:hover {
+      .cardButton {
+        top: 0 !important;
       }
     }
   }
- 
 
-  .cardContent {
-    padding: 20px;
+  .cardContainer {
     display: flex;
     flex-direction: column;
 
-   
-        
-    
-    .cardContent_header {
+    .cardImg {
       width: 100%;
+      height: 300px;
+      overflow: hidden;
 
-      .contentLeft {
-        .title {
-          width: 350px;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          font-size: 1rem;
-          text-transform: uppercase;
-          letter-spacing: 5px;
-          font-weight: 700;
-        }
-      }
-      .contentRight {
-        align-self: center;
-        .price {
-          width: 100%;
-          text-overfow: ellipsis;
-          font-size: 1.5rem;
-          font-weight: 700;
-          text-transform: uppercase;
+      .img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+
+        &:hover {
+          transform: scale(1.1);
+          transition: all 0.5s ease;
         }
       }
     }
-    
-    .hiddenButtonBox:hover > .cardButton {
-      top: 0;
-    }
-    .cardButton {
-      color: lightgreen;
-      font-size: 1.2rem;
-      text-transform: uppercase;
-      font-weight: 700;
-      transition: all 0.3s ease-out;
-      top: 170%;
-      position: relative;
-      height: 100%;
-      &:hover {
-        background-color: lightgreen;
-        color: white;
+
+    .cardContent {
+      padding: 20px;
+      display: flex;
+      flex-direction: column;
+
+      .cardContent_header {
+        width: 100%;
+
+        .contentLeft {
+          .title {
+            width: 350px;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            font-size: 1rem;
+            text-transform: uppercase;
+            letter-spacing: 5px;
+            font-weight: 700;
+          }
+        }
+        .contentRight {
+          align-self: center;
+          .price {
+            width: 100%;
+            text-overfow: ellipsis;
+            font-size: 1.5rem;
+            font-weight: 700;
+            text-transform: uppercase;
+          }
+        }
+      }
+
+      .hiddenButtonBox:hover > .cardButton {
+        top: 0;
+      }
+      .cardButton {
+        color: lightgreen;
+        font-size: 1.2rem;
+        text-transform: uppercase;
+        font-weight: 700;
+        transition: all 0.3s ease-out;
+        top: 170%;
+        position: relative;
+        height: 100%;
+        &:hover {
+          background-color: lightgreen;
+          color: white;
+        }
       }
     }
   }
-}
 `;
 
 export default function MyNFTInfo({
@@ -127,13 +118,15 @@ export default function MyNFTInfo({
   const [desc, setDesc] = useState("");
 
   const transNFT = async () => {
-    const a = await (await marketplaceContract.transItem(nftContract.address, id)).wait();
-    console.log(a)
+    const a = await (
+      await marketplaceContract.transItem(nftContract.address, id)
+    ).wait();
+    console.log(a);
     const trans = {
       trans_id: id,
       account_address: account,
       amount: 0,
-      type: 0,  //0: donate nft  |  1: donate eth  | 2: auction nft
+      type: 0, //0: donate nft  |  1: donate eth  | 2: auction nft
       description: desc,
     };
     await api.createTrans(trans);
@@ -149,86 +142,80 @@ export default function MyNFTInfo({
     setOpen(false);
     setDesc("");
   };
-  
- 
 
   return (
     <NFTItemStyled>
       <Box className="wrapper">
-
-      <Box
-        className="cardContainer"
-        
-        >
-        <Box className="cardImg">
-          <img
-            className="img"
-            src={`data:image/png;base64,${toBase64(image.buffer.data)}`}
-            alt="nft_img"
+        <Box className="cardContainer">
+          <Box className="cardImg">
+            <img
+              className="img"
+              src={`data:image/png;base64,${toBase64(image.buffer.data)}`}
+              alt="nft_img"
             />
-        </Box>
-        <Box className="cardContent">
-          <Box className="cardContent_header">
-            <Box className="contentLeft">
-              {/* <Typography className="id">#{id}</Typography> */}
-              <Typography className="title" gutterBottom>
-                {name}
-                {"  #"}
-                {id}
-              </Typography>
-            </Box>
-            <Box className="contentRight">
-              <Typography className="price">{price} ETH</Typography>
-            </Box>
           </Box>
-          <Box height={30} className="hiddenButtonBox">
+          <Box className="cardContent">
+            <Box className="cardContent_header">
+              <Box className="contentLeft">
+                {/* <Typography className="id">#{id}</Typography> */}
+                <Typography className="title" gutterBottom>
+                  {name}
+                  {"  #"}
+                  {id}
+                </Typography>
+              </Box>
+              <Box className="contentRight">
+                <Typography className="price">{price} ETH</Typography>
+              </Box>
+            </Box>
+            <Box height={30} className="hiddenButtonBox">
               <Button
                 className="cardButton"
                 variant="outlined"
                 fullWidth
                 onClick={handleClickOpen}
-                >
+              >
                 Donate this NFT
               </Button>
+            </Box>
           </Box>
-        </Box>
 
-        {/* the dialog will appear when we click button donate */}
-        <Dialog keepMounted open={open} onClose={handleClose}>
-          <DialogTitle>Transform NFT To System</DialogTitle>
-          <DialogContent>
-            <DialogContentText>
-              To transform NFT please enter short description and submit. We
-              thank you for your contribution.
-            </DialogContentText>
-            <TextField
-              margin="dense"
-              label="NFT Name"
-              value={name}
-              fullWidth
-              variant="standard"
-              sx={{ marginTop: "20px" }}
+          {/* the dialog will appear when we click button donate */}
+          <Dialog keepMounted open={open} onClose={handleClose}>
+            <DialogTitle>Transform NFT To System</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To transform NFT please enter short description and submit. We
+                thank you for your contribution.
+              </DialogContentText>
+              <TextField
+                margin="dense"
+                label="NFT Name"
+                value={name}
+                fullWidth
+                variant="standard"
+                sx={{ marginTop: "20px" }}
               />
-            <TextField
-              autoFocus
-              margin="dense"
-              label="Short Description"
-              value={desc}
-              onChange={(e) => {
-                setDesc(e.target.value);
-              }}
-              fullWidth
-              variant="standard"
-              sx={{ marginTop: "10px" }}
+              <TextField
+                autoFocus
+                margin="dense"
+                label="Short Description"
+                value={desc}
+                onChange={(e) => {
+                  setDesc(e.target.value);
+                }}
+                fullWidth
+                variant="standard"
+                sx={{ marginTop: "10px" }}
               />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-            <Button onClick={transNFT}>Submit</Button>
-          </DialogActions>
-        </Dialog>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={transNFT}>Submit</Button>
+            </DialogActions>
+          </Dialog>
+        </Box>
       </Box>
-              </Box>
     </NFTItemStyled>
   );
 }
