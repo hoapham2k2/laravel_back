0c33095c4141e4c9f9d69ccef1a0e4d491d679ef
hoapham <20520995@gm.uit.edu.vn>
2022-12-12 11:39:58 +0700

    fix 404

--- src/pages/ErrorPages.jsx
+++ src/pages/ErrorPages.jsx
@@ -1,22 +1,48 @@
 import React from "react";
+import styled from "@emotion/styled";
+import sad from "../assets/img/sad.svg";
+const StyledErrorPages = styled.div`
+  width: 100%;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  h1 {
+    font-weight: 700;
+    font-size: 3rem;
+    line-height: 1rem;
+    text-transform: uppercase;
+  }
+
+  .errorPages {
+    min-width: 300px;
+    min-height: 30vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .sorryImg {
+    width: 100px;
+    height: 100px;
+  }
+`;
 
 const ErrorPages = () => {
   return (
-    <div>
-      <h1>404</h1>;
-      <div className="cloak__wrapper">
-        <div className="cloak__container">
-          <div className="cloak"></div>
-        </div>
-      </div>
-      <div className="info">
-        <h2>We can't find that page </h2>
-        <p>
+    <StyledErrorPages>
+      <div className="errorPages">
+        <img src={sad} alt="Sorry_about_that" className="sorryImg" />
+        <h1>sorry</h1>
+        <h1 style={{ textTransform: "uppercase" }}>We can't find that page </h1>
+        <p style={{ textAlign: "justify" }}>
           We're fairly sure that page used to be here, but seems to have gone
           missing. We do apologise on it's behalf.
         </p>
       </div>
-    </div>
+    </StyledErrorPages>
   );
 };
 
